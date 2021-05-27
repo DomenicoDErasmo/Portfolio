b54ae820e6fa3851c16be6f070d714f0e1f5789d
DomenicoDErasmo <44101761+DomenicoDErasmo@users.noreply.github.com>
2021-05-27 17:28:06 -0400

    Updated SongRanker
    Built mock GUI for Song Comparison

--- Projects/SongRanker/Testing/GuiMenu.py
+++ Projects/SongRanker/Testing/GuiMenu.py
@@ -1,6 +1,8 @@
 import os
+from pathlib import Path
 import tkinter as tk
 import random
+from PIL import Image, ImageTk
 
 from CustomModules import FileReader, SongFileReader, SongUpdater, MenuDisplay, SongComparison
 
@@ -34,11 +36,107 @@ class GuiMenu:
             {
                 "bg": "orchid1",
                 "height": 3,
-                "width": 20
+                "width": 20,
+                "wraplength": 100
             }
 
+        self.label_properties = \
+            {
+                "bg": "light goldenrod",
+                "height": 3,
+                "width": 20,
+                "wraplength": 100
+            }
+
+    def GetTwoRandomSongs(self):
+        """
+        Randomly selects two songs for the user to compare.
+
+        Returned Values:
+            left_song_data -- Data for the first song chosen.
+            right_song_data -- Data for the second song chosen.
+
+            Both left_song_data and right_song_data are structs consisting of the following fields of data:
+                song_name -- The name of the song.
+                album -- The album the song comes from.
+                artist -- The artist that performs the song.
+                elo -- The ELO score of the song, used for ranking songs.
+        """
+        # Determine two songs to compare based on line number
+        left_song_pos = random.randrange(0, len(self.songs.list_of_lines) - 1, 1)
+        right_song_pos = random.randrange(0, len(self.songs.list_of_lines) - 1, 1)
+
+        # Ensures two distinct numbers
+        while right_song_pos == left_song_pos:
+            right_song_pos = random.randrange(0, len(self.songs.list_of_lines) - 1, 1)
+
+        # Parse data for the two songs
+        left_song_data = self.songs.list_of_lines[left_song_pos].split('~')
+        right_song_data = self.songs.list_of_lines[right_song_pos].split('~')
+        both_songs = [left_song_data, right_song_data]
+
+        return both_songs
+
+    def SelectLeftSong(self):
+        print("Chose left song")
+
+    def SelectRightSong(self):
+        print("Chose right song")
+
+    def RankSongs(self):
+        menu = tk.Toplevel()
+        menu.geometry("500x500")
+
+        both_songs = self.GetTwoRandomSongs()
+        left_image = os.path.splitext(both_songs[0][1])[0]
+        right_image = os.path.splitext(both_songs[1][1])[0]
+        left_path = Path(os.getcwd()).parent / "ResourceFiles\\Album Covers" / (left_image + ".jpg")
+        right_path = Path(os.getcwd()).parent / "ResourceFiles\\Album Covers" / (right_image + ".jpg")
+        left_img = ImageTk.PhotoImage(Image.open(left_path).resize((100, 100)))
+        right_img = ImageTk.PhotoImage(Image.open(right_path).resize((100, 100)))
+        left_panel = tk.Label(menu, image=left_img)
+        right_panel = tk.Label(menu, image=right_img)
+        # left_panel.place(x=0, y=0)
+        # right_panel.place(x=351, y=0)
+
+        # TODO: research placing in frames/nested frames
+        # TODO: research how to rebuild window
+
+        left_song_button = tk.Button(menu, **self.button_properties, text=both_songs[0][0], command=self.SelectLeftSong)
+        right_song_button = \
+            tk.Button(menu, **self.button_properties, text=both_songs[1][0], command=self.SelectRightSong)
+
+
+        # left_song_button.place(x=0, y=200)
+        # right_song_button.place(x=351, y=200)
+
+        header_frame = tk.Frame(menu, bg="light blue", width=500, height=100)
+        header_frame.place(x=0, y=0)
+
+        left_song_frame = tk.Frame(menu, bg="pale green", width=250, height=300)
+        left_song_frame.place(x=0, y=100)
+
+        left_song_info = \
+            tk.Label(left_song_frame, **self.label_properties, text=both_songs[0][2] + ": " + both_songs[0][1])
+        left_song_info.place(x=0, y=160)
+
+        left_song_button = \
+            tk.Button(left_song_frame, **self.button_properties, text=both_songs[0][0], command=self.SelectLeftSong)
+        left_song_button.place(x=0, y=210)
+
+        right_song_frame = tk.Frame(menu, bg="pale green", width=250, height=300)
+        right_song_frame.place(x=250, y=100)
+
+        footer_frame = tk.Frame(menu, bg="light blue", width=500, height=100)
+        footer_frame.place(x=0, y=400)
+
+        exit_button = tk.Button(footer_frame, **self.button_properties, text="Exit", command=menu.destroy)
+        exit_button.place(x=351, y=45)
+
+        menu.mainloop()
+
     def BuildButtons(self, gui):
-        rank_songs = tk.Button(gui, **self.button_properties, text="Rank Songs", )
+        rank_songs = tk.Button(gui, **self.button_properties, text="Rank Songs", command=self.RankSongs)
         change_k_value = tk.Button(gui, **self.button_properties, text="Change K Value", )
         display_top_songs = tk.Button(gui, **self.button_properties, text="Display Top Songs", )
         view_song = tk.Button(gui, **self.button_properties, text="View Song", )
