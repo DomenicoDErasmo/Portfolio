056551a80a437a67887d01e508838ed389615625
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2021-06-15 10:20:30 -0400

    Completed ShortestSubstring

--- LeetCode/LeetCode Studying/src/Problem.java
+++ LeetCode/LeetCode Studying/src/Problem.java
@@ -2,6 +2,10 @@ import java.util.*;
 
 abstract public class Problem
 {
+	public Problem()
+	{
+		
+	}
 	public abstract void GenerateArguments();
 	public abstract void SolveProblem ();
 	protected ArrayList <Object> args;
