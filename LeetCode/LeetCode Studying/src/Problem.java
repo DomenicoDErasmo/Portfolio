6f6b17bfd169a83af650f74fcf273a3f73478e2d
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2021-06-15 10:27:05 -0400

    Revert "Completed ShortestSubstring"
    This reverts commit 056551a80a437a67887d01e508838ed389615625.

--- LeetCode/LeetCode Studying/src/Problem.java
+++ LeetCode/LeetCode Studying/src/Problem.java
@@ -2,10 +2,6 @@ import java.util.*;
 
 abstract public class Problem
 {
-	public Problem()
-	{
-		
-	}
 	public abstract void GenerateArguments();
 	public abstract void SolveProblem ();
 	protected ArrayList <Object> args;
